e588ffe1afadcfa70129aa8981f0f710c6af5b24
Turtlepaw <turtlesimsyt@gmail.com>
2022-09-09 21:42:28 +0000

    create dev branch

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,16 @@ import { useState } from 'react';
 
 const Home: NextPage = () => {
   const [search, setSearch] = useState('');
+  const [results, setResults] = useState<{
+    id: string;
+    name: string;
+    avatar: string;
+  }[]>([]);
+  async function searchValue(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(e.target.value);
+    const r = await Client.findByUsername(e.target.value)
+    setResults(r)
+  }
 
   return (
     <div className="container">
@@ -15,7 +25,15 @@ const Home: NextPage = () => {
         <h1 className="text-5xl font-bold">
           <SearchIcon /> Search for Players
         </h1>
-        <Input onChange={e => setSearch(e.target.value)} className="searchInput mt-10" width="500px"/>
+        <Input onChange={searchValue} className="searchInput mt-10" width="500px"/>
+        <div className="results">
+          {results.map(result => (
+            <div className="result" key={result.id}>
+              <Image src={result.avatar} alt={result.name} width="100px" height="100px" />
+              <div className="name">{result.name}</div>
+            </div>
+          ))}
+        </div>
       </Center>
     </div>
   )
--- util/stats.ts
+++ util/stats.ts
@@ -1,4 +1,61 @@
-import R6API from "r6api.js";
+import R6API, { typings } from "r6api.js";
 
+export interface Player {
+    id: string;
+    userId: string;
+    idOnPlatform: string;
+    platform: "uplay" | "psn" | "xbl" | "steam" | "epic" | "amazon";
+    username: string;
+    avatar: {
+        146: string;
+        256: string;
+        500: string;
+    };
+}
 const { UBI_EMAIL: email = '', UBI_PASSWORD: password = '' } = process.env;
 export const Client = new R6API({ email, password });
+
+export async function getPlayerFromAllPlatforms(name: string): Promise<(Player[])> {
+    const platforms: ("uplay" | "psn" | "xbl" | "steam" | "epic" | "amazon")[] = [
+        "uplay",
+        "psn",
+        "xbl",
+        "steam",
+        "epic",
+        "amazon"
+    ]
+    const player = [];
+    for (const platform of platforms) {
+        const playerData = await Client.findByUsername(platform, name);
+        if (playerData) {
+            player.push(playerData[0]);
+        }
+    }
+
+    return player;
+}
+
+export async function findByName(name: string) {
+    //Generate all possible names for the player
+    const names = [
+        name,
+        name.toLowerCase(),
+        name.toUpperCase(),
+        name.charAt(0).toUpperCase() + name.slice(1).toLowerCase(),
+        name.charAt(0).toLowerCase() + name.slice(1).toUpperCase()
+    ];
+
+    const results: Player[] = [];
+    names.forEach(
+        async (name) => {
+            const players = await getPlayerFromAllPlatforms(name);
+
+            if (players.length > 0) {
+                players.forEach((player) => {
+                    results.push(player);
+                });
+            }
+        }
+    )
+    return results;   
+}
